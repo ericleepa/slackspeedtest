be343fa7d3be71b847d017938301bd9be0de7934
ericleepa <eric.lee.pa@gmail.com>
2016-06-08 07:39:49 -0400

    Formatting result of speedtest

--- manager.py
+++ manager.py
@@ -1,6 +1,21 @@
 import os
 import config
+from datetime import datetime
 
-result = os.popen(config.speedtest_cli_dir).read()
 
-print(result)
+def doSpeedTest():
+
+    result = os.popen(config.speedtest_cli_dir).read()
+
+    resultSet = result.split('\n')
+    pingResult = resultSet[0]
+    downloadResult = resultSet[1]
+    uploadResult = resultSet[2]
+
+    pingResult = float(pingResult.replace('Ping: ', '').replace(' ms', ''))
+    downloadResult = float(downloadResult.replace('Download: ', '').replace(' Mbit/s', ''))
+    uploadResult = float(uploadResult.replace('Upload: ', '').replace(' Mbit/s', ''))
+
+    print(pingResult, downloadResult, uploadResult)
+
+doSpeedTest()
