31723d60c318beee923b29e13f55c22c922a7a8f
ericleepa <eric.lee.pa@gmail.com>
2016-06-12 10:51:31 -0400

    Speedtest_bot function
    Created function to build json data and deliver it to speedtest_bot.py.
    Renamed files (changed case, removed hypen)

--- manager.py
+++ manager.py
@@ -1,10 +1,9 @@
 import os
 import config
-from datetime import datetime
+import speedtest_bot
 
 
 def doSpeedTest():
-
     result = os.popen(config.speedtest_cli_dir).read()
 
     resultSet = result.split('\n')
@@ -12,10 +11,24 @@ def doSpeedTest():
     downloadResult = resultSet[1]
     uploadResult = resultSet[2]
 
-    pingResult = float(pingResult.replace('Ping: ', '').replace(' ms', ''))
-    downloadResult = float(downloadResult.replace('Download: ', '').replace(' Mbit/s', ''))
-    uploadResult = float(uploadResult.replace('Upload: ', '').replace(' Mbit/s', ''))
+    pingfloat = float(pingResult.replace('Ping: ', '').replace(' ms', ''))
+    downloadfloat = float(downloadResult.replace('Download: ', '').replace(' Mbit/s', ''))
+    uploadfloat = float(uploadResult.replace('Upload: ', '').replace(' Mbit/s', ''))
 
     print(pingResult, downloadResult, uploadResult)
+    buildjson(downloadResult, uploadResult, pingResult)
+
+
+def buildjson(down, up, ping):
+    test_to_string = ("%s  \n%s \n%s" % (down, up, ping))
+    list = []
+    print(test_to_string)
+    test = {'text': test_to_string, 'color': 'good', 'fallback': 'No speedtest data', 'title': 'Speedtest.net',
+            'pretext': 'Speed test results', 'title_link': 'https://speedtest.net/run'}
+    list.append(test)
+    payload = {'attachments': list}
+    print(payload)
+    speedtest_bot.post_to_slack(payload)
+
 
 doSpeedTest()
